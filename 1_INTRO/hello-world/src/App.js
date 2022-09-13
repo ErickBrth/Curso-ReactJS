a83e25dc4d2437eeec1d2295500b2c750544aee0
ErickBrth <erickaraken@gmail.com>
2022-09-13 12:57:07 -0300

    "hello world"

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Erick Brth - Computer Scientist</h1>
+    <h3>My First Hello world react!!</h3>
+    <h3>Emmet eh massa!</h3>
+    <a href="https://www.linkedin.com/in/erick-araken-vieira-gomes-030654207/">
+      Meu LinkedIn
+    </a>
+    </div>
+  );
+  
+}
+
+export default App;
