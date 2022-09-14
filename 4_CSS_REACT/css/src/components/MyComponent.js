f3155d93433e11f36ff5434b96eda1f13ed14643
ErickBrth <erickaraken@gmail.com>
2022-09-13 21:53:10 -0300

    css

---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p>Este parágrafo vai ter um estilo</p>
+      <p className="my-comp-paragraph">Este tmb é do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
